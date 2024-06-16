13ab8efa048da2eb1c54d84ab28c7ff6404f3aa6
김멍몽 <wjdahrrla@gmail.com>
2024-06-16 22:31:38 +0900

    refactor: Diary 엔티티 리팩토링 - #mung

--- src/main/java/moodbuddy/moodbuddy/domain/diary/entity/Diary.java
+++ src/main/java/moodbuddy/moodbuddy/domain/diary/entity/Diary.java
@@ -1,4 +1,23 @@
 package moodbuddy.moodbuddy.domain.diary.entity;
 
-public class Diary {
+import jakarta.persistence.*;
+import lombok.Getter;
+import moodbuddy.moodbuddy.domain.user.entity.User;
+import moodbuddy.moodbuddy.global.common.base.BaseEntity;
+
+@Entity
+@Getter
+public class Diary extends BaseEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "diary_id")
+    private Long id;
+    private String diaryTitle;
+    private String diaryContent;
+    private DiaryWeather diaryWeather;
+    private DiaryEmotion diaryEmotion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "diary_id")
+    private User user;
 }
---
+++ src/main/java/moodbuddy/moodbuddy/domain/diary/entity/DiaryEmotion.java
@@ -0,0 +1,5 @@
+package moodbuddy.moodbuddy.domain.diary.entity;
+
+public enum DiaryEmotion {
+    // 7개 이모션 넣어야 함.
+}
