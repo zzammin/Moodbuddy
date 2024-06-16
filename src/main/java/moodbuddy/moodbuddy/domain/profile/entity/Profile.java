085daefe2a75807063e4db392c5cbac677ce2db7
김멍몽 <wjdahrrla@gmail.com>
2024-06-16 22:18:38 +0900

    refactor: Profile 엔티티 리팩토링 - #mung

--- src/main/java/moodbuddy/moodbuddy/domain/profile/entity/Profile.java
+++ src/main/java/moodbuddy/moodbuddy/domain/profile/entity/Profile.java
@@ -1,4 +1,21 @@
 package moodbuddy.moodbuddy.domain.profile.entity;
 
-public class Profile {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import moodbuddy.moodbuddy.global.common.base.BaseEntity;
+
+@Entity
+@Getter
+public class Profile extends BaseEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "profile_id")
+    private Long id;
+    private String profileNickName;
+    private String profileComment = "";
+    /** 생성자 **/
+    protected Profile() {}
 }
