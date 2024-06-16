dba39c9bb9b76426e5c050e8e59c36b95b0ede3e
김멍몽 <wjdahrrla@gmail.com>
2024-06-16 22:24:11 +0900

    refactor: MonthlyStatistics 엔티티 리팩토링 - #mung

--- src/main/java/moodbuddy/moodbuddy/domain/monthlyStatictics/entity/MonthlyStatistics.java
+++ src/main/java/moodbuddy/moodbuddy/domain/monthlyStatictics/entity/MonthlyStatistics.java
@@ -1,4 +1,19 @@
 package moodbuddy.moodbuddy.domain.monthlyStatictics.entity;
 
-public class MonthlyStatistics {
+import jakarta.persistence.*;
+import lombok.Getter;
+import moodbuddy.moodbuddy.domain.user.entity.User;
+import moodbuddy.moodbuddy.global.common.base.BaseEntity;
+
+@Entity
+@Getter
+public class MonthlyStatistics extends BaseEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "monthly_statistics_id")
+    private Long id;
+    @OneToOne(mappedBy = "monthly_statistics", fetch = FetchType.LAZY, orphanRemoval = true)
+    private User user;
+
+    // 월별 통계 잠시 스탑
 }
