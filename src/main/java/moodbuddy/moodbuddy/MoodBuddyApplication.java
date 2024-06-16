14863122b0eb95c6151e3867445c0a6b7ab613c0
김멍몽 <wjdahrrla@gmail.com>
2024-06-16 18:30:26 +0900

    chore: Dockerfile 추가 - #mung

---
+++ src/main/java/moodbuddy/moodbuddy/MoodBuddyApplication.java
@@ -0,0 +1,13 @@
+package moodbuddy.moodbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoodBuddyApplication.class, args);
+    }
+
+}
