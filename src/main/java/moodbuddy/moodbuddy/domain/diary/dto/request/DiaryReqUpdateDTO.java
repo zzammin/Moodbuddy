6aac51a124932c01c5b8efb2108bc223a1ec06e9
김멍몽 <wjdahrrla@gmail.com>
2024-06-23 00:35:01 +0900

    feat: DiaryReqUpdateDTO 데이터 추가 - #mung

--- src/main/java/moodbuddy/moodbuddy/domain/diary/dto/request/DiaryReqUpdateDTO.java
+++ src/main/java/moodbuddy/moodbuddy/domain/diary/dto/request/DiaryReqUpdateDTO.java
@@ -1,5 +1,15 @@
 package moodbuddy.moodbuddy.domain.diary.dto.request;
 
-public class DiaryReqUpdateDTO
-{
+import moodbuddy.moodbuddy.domain.diary.entity.DiaryWeather;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class DiaryReqUpdateDTO {
+    private String diaryTitle;
+    private LocalDateTime diaryDate;
+    private String diaryContent;
+    private DiaryWeather diaryWeather;
+    private List<MultipartFile> diaryImgList;
 }
