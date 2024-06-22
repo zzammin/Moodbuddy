2a28a6d2832d6ba25dfa4a6e15a9dd8def7c0f21
김멍몽 <wjdahrrla@gmail.com>
2024-06-22 22:44:12 +0900

    refactor: Diary 엔티티 @Column을 통해 자세하게 저장 - #mung

--- src/main/java/moodbuddy/moodbuddy/domain/diary/entity/Diary.java
+++ src/main/java/moodbuddy/moodbuddy/domain/diary/entity/Diary.java
@@ -2,23 +2,36 @@ package moodbuddy.moodbuddy.domain.diary.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import moodbuddy.moodbuddy.domain.user.entity.User;
 import moodbuddy.moodbuddy.global.common.base.BaseEntity;
 
 @Entity
 @Getter
 public class Diary extends BaseEntity {
+
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "diary_id")
     private Long id;
+
+    @Column(name = "diary_title", nullable = false, length = 255)
     private String diaryTitle;
+
+    @Lob
+    @Column(name = "diary_content")
     private String diaryContent;
+
     @Enumerated(EnumType.STRING)
+    @Column(name = "diary_weather", nullable = false, length = 255)
     private DiaryWeather diaryWeather;
+
     @Enumerated(EnumType.STRING)
+    @Column(name = "diary_emotion", nullable = false, length = 255)
     private DiaryEmotion diaryEmotion;
+
+    @Column(name = "diary_summary", nullable = false, length = 255)
     private String diarySummary;
+
+    @Column(name = "user_email", nullable = false, length = 255)
     private String userEmail; // ManyToOne으로 User와 Diary를 연결하는 것이 아닌, userEmail을 칼럼으로 넣음
 
 //    @ManyToOne(fetch = FetchType.LAZY)
