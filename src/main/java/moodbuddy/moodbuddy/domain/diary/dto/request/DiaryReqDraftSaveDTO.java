c5f4a519e195a1f8424c738a2f92f9bdf621765c
김멍몽 <wjdahrrla@gmail.com>
2024-06-23 00:34:42 +0900

    feat: DiaryReqDraftSaveDTO 데이터 추가 - #mung

--- src/main/java/moodbuddy/moodbuddy/domain/diary/dto/request/DiaryReqDraftSaveDTO.java
+++ src/main/java/moodbuddy/moodbuddy/domain/diary/dto/request/DiaryReqDraftSaveDTO.java
@@ -1,4 +1,15 @@
 package moodbuddy.moodbuddy.domain.diary.dto.request;
 
+import moodbuddy.moodbuddy.domain.diary.entity.DiaryWeather;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class DiaryReqDraftSaveDTO {
+    private String diaryTitle;
+    private LocalDateTime diaryDate;
+    private String diaryContent;
+    private DiaryWeather diaryWeather;
+    private List<MultipartFile> diaryImgList;
 }
