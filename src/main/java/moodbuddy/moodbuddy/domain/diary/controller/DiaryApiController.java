c493638ac595d81daca7cdeac8dd50bf3aff075f
김멍몽 <wjdahrrla@gmail.com>
2024-06-23 22:42:41 +0900

    feat: DiaryApiController update 구현 - #mung

--- src/main/java/moodbuddy/moodbuddy/domain/diary/controller/DiaryApiController.java
+++ src/main/java/moodbuddy/moodbuddy/domain/diary/controller/DiaryApiController.java
@@ -3,15 +3,14 @@ package moodbuddy.moodbuddy.domain.diary.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import moodbuddy.moodbuddy.domain.diary.dto.request.DiaryReqSaveDTO;
+import moodbuddy.moodbuddy.domain.diary.dto.request.DiaryReqUpdateDTO;
 import moodbuddy.moodbuddy.domain.diary.dto.response.DiaryResSaveDTO;
+import moodbuddy.moodbuddy.domain.diary.dto.response.DiaryResUpdateDTO;
 import moodbuddy.moodbuddy.domain.diary.service.DiaryServiceImpl;
 import moodbuddy.moodbuddy.global.common.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/member/diary")
@@ -20,13 +19,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class DiaryApiController {
     private final DiaryServiceImpl diaryService;
     @PostMapping("/save")
-    public ResponseEntity<?> save(@ModelAttribute DiaryReqSaveDTO diaryReqSaveDTO) {
+    public ResponseEntity<?> save(@RequestBody DiaryReqSaveDTO diaryReqSaveDTO) {
         log.info("[DiaryApiController] save");
         try {
             DiaryResSaveDTO result = diaryService.save(diaryReqSaveDTO);
             return ResponseEntity.ok().body(ApiResponse.SUCCESS(HttpStatus.CREATED.value(), "[SUCCESS] DiaryApiController save", result));
         } catch (Exception e) {
             log.error("[DiaryApiController] error", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()));
+        }
+    }
+
+    @PatchMapping("/update")
+    public ResponseEntity<?> update(@RequestBody DiaryReqUpdateDTO diaryReqUpdateDTO) {
+        log.info("[DiaryApiController] update");
+        try {
+            DiaryResUpdateDTO result = diaryService.update(diaryReqUpdateDTO);
+            return ResponseEntity.ok().body(ApiResponse.SUCCESS(HttpStatus.CREATED.value(), "[SUCCESS] DiaryApiController update", result));
+        } catch (Exception e) {
+            log.error("[DiaryApiController] update", e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()));
         }
     }
