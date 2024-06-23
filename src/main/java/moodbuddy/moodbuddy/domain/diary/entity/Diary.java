9bb9e406dd2696486b7fbfb4a2e42ab4544ca9a5
김멍몽 <wjdahrrla@gmail.com>
2024-06-23 22:15:07 +0900

    refactor: Diary @AllArgsConstructor, @NoArgsContructor 추가 - #mung

--- src/main/java/moodbuddy/moodbuddy/domain/diary/entity/Diary.java
+++ src/main/java/moodbuddy/moodbuddy/domain/diary/entity/Diary.java
@@ -1,8 +1,7 @@
 package moodbuddy.moodbuddy.domain.diary.entity;
 
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 import moodbuddy.moodbuddy.global.common.base.BaseEntity;
 
 import java.time.LocalDateTime;
@@ -10,6 +9,8 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Diary extends BaseEntity {
 
     @Id
@@ -44,10 +45,6 @@ public class Diary extends BaseEntity {
 
     @Column(name = "user_email", nullable = false, length = 255)
     private String userEmail; // ManyToOne으로 User와 Diary를 연결하는 것이 아닌, userEmail을 칼럼으로 넣음
-
-    protected Diary() {
-
-    }
 
 //    @ManyToOne(fetch = FetchType.LAZY)
 //    @JoinColumn(name = "user_id")
