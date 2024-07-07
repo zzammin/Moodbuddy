acaa88264df931caee54fc9b8cf04802341f0025
dylee00 <dylee6820@gmail.com>
2024-07-07 23:23:58 +0900

    Refactor: db변경에 따른 entity, repository 변경

--- src/main/java/moodbuddy/moodbuddy/domain/profile/entity/Profile.java
+++ src/main/java/moodbuddy/moodbuddy/domain/profile/entity/Profile.java
@@ -1,12 +1,17 @@
 package moodbuddy.moodbuddy.domain.profile.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import moodbuddy.moodbuddy.domain.user.entity.User;
 import moodbuddy.moodbuddy.global.common.base.BaseEntity;
 
 @Entity
 @Getter
+@SuperBuilder
 public class Profile extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,9 +24,8 @@ public class Profile extends BaseEntity {
     @Column(name = "profile_comment", columnDefinition = "varchar(255)")
     private String profileComment = "";
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+    @Column(name = "kakao_id", columnDefinition = "bigint")
+    private Long kakaoId;
 
     protected Profile() {}
 }
