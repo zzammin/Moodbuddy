d8c4333b4b0b46bb650859546c487f11379a1269
김멍몽 <wjdahrrla@gmail.com>
2024-07-14 19:07:36 +0900

    feat: Exception에 @ResponseStatus 추가 - #mung

--- src/main/java/moodbuddy/moodbuddy/global/common/exception/diary/DiaryNoAccessException.java
+++ src/main/java/moodbuddy/moodbuddy/global/common/exception/diary/DiaryNoAccessException.java
@@ -1,7 +1,12 @@
 package moodbuddy.moodbuddy.global.common.exception.diary;
 
+import lombok.Getter;
 import moodbuddy.moodbuddy.global.common.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.FORBIDDEN)
+@Getter
 public class DiaryNoAccessException extends RuntimeException {
     private final ErrorCode errorCode;
     public DiaryNoAccessException(ErrorCode errorCode) {
--- src/main/java/moodbuddy/moodbuddy/global/common/exception/quddyTI/QuddyTINotFoundException.java
+++ src/main/java/moodbuddy/moodbuddy/global/common/exception/quddyTI/QuddyTINotFoundException.java
@@ -2,6 +2,11 @@ package moodbuddy.moodbuddy.global.common.exception.quddyTI;
 
 import lombok.Getter;
 import moodbuddy.moodbuddy.global.common.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+@Getter
 public class QuddyTINotFoundException extends RuntimeException {
     private final ErrorCode errorCode;
     public QuddyTINotFoundException(ErrorCode errorCode) {
